b1341f4851d5b5587318ba7f1274ea478b2e0ecd
Juan Antonio Cano <jacanosalado@gmail.com>
2018-12-15 17:38:30 +0100

    upd sniffer

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,93 @@
+﻿using PcapDotNet.Core;
+using PcapDotNet.Packets;
+using PcapDotNet.Packets.IpV4;
+using PcapDotNet.Packets.Transport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Retrieve the device list from the local machine
+            var allDevices = LivePacketDevice.AllLocalMachine;
+
+            if (allDevices.Count == 0)
+            {
+                Console.WriteLine("No interfaces found! Make sure WinPcap is installed.");
+                return;
+            }
+
+            // Print the list
+            for (int i = 0; i != allDevices.Count; ++i)
+            {
+                LivePacketDevice device = allDevices[i];
+
+                var addrs = string.Join(", ", device.Addresses.Select(x => x.Address));
+
+                Console.Write((i + 1) + ". " + device.Name + " addrs: " + addrs);
+
+                if (device.Description != null)
+                    Console.WriteLine(" (" + device.Description + ")");
+                else
+                    Console.WriteLine(" (No description available)");
+            }
+
+            int deviceIndex = 0;
+            do
+            {
+                Console.WriteLine("Enter the interface number (1-" + allDevices.Count + "):");
+                string deviceIndexString = Console.ReadLine();
+                if (!int.TryParse(deviceIndexString, out deviceIndex) ||
+                    deviceIndex < 1 || deviceIndex > allDevices.Count)
+                {
+                    deviceIndex = 0;
+                }
+            } while (deviceIndex == 0);
+
+            // Take the selected adapter
+            PacketDevice selectedDevice = allDevices[deviceIndex - 1];
+
+            // Open the device
+            using (PacketCommunicator communicator =
+                selectedDevice.Open(65536,                                  // portion of the packet to capture
+                                                                            // 65536 guarantees that the whole packet will be captured on all the link layers
+                                    PacketDeviceOpenAttributes.Promiscuous, // promiscuous mode
+                                    1000))                                  // read timeout
+            {
+                // Check the link layer. We support only Ethernet for simplicity.
+                if (communicator.DataLink.Kind != DataLinkKind.Ethernet)
+                {
+                    Console.WriteLine("This program works only on Ethernet networks.");
+                    return;
+                }
+
+                // Compile the filter
+                using (BerkeleyPacketFilter filter = communicator.CreateFilter("ip and udp"))
+                {
+                    // Set the filter
+                    communicator.SetFilter(filter);
+                }
+
+                Console.WriteLine("Listening on " + selectedDevice.Description + "...");
+
+                // start the capture
+                communicator.ReceivePackets(0, PacketHandler);
+            }
+        }
+
+        // Callback function invoked by libpcap for every incoming packet
+        private static void PacketHandler(Packet packet)
+        {
+            IpV4Datagram ip = packet.Ethernet.IpV4;
+            UdpDatagram udp = ip.Udp;
+
+            // print ip addresses and udp ports
+            Console.WriteLine(packet.Timestamp.ToString("yyyy-MM-dd hh:mm:ss.fff") + " " + ip.Source + ":" + udp.SourcePort + " -> " + ip.Destination + ":" + udp.DestinationPort);
+            Console.WriteLine(udp.Payload);
+        }
+    }
+}
