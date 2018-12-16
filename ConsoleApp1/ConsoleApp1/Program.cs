e3236903773d1fabaefa643f7e4470cb0eb84ab9
Juan Antonio Cano <jacanosalado@gmail.com>
2018-12-16 20:15:28 +0100

    IceKey test added

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,9 +1,6 @@
 ﻿using PcapDotNet.Core;
 using PcapDotNet.Packets;
-using PcapDotNet.Packets.IpV4;
-using PcapDotNet.Packets.Transport;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace ConsoleApp1
@@ -12,19 +9,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // Retrieve the device list from the local machine
             var allDevices = LivePacketDevice.AllLocalMachine;
-
             if (allDevices.Count == 0)
             {
                 Console.WriteLine("No interfaces found! Make sure WinPcap is installed.");
                 return;
             }
 
-            // Print the list
             for (int i = 0; i != allDevices.Count; ++i)
             {
-                LivePacketDevice device = allDevices[i];
+                var device = allDevices[i];
 
                 var addrs = string.Join(", ", device.Addresses.Select(x => x.Address));
 
@@ -40,7 +34,7 @@ namespace ConsoleApp1
             do
             {
                 Console.WriteLine("Enter the interface number (1-" + allDevices.Count + "):");
-                string deviceIndexString = Console.ReadLine();
+                var deviceIndexString = Console.ReadLine();
                 if (!int.TryParse(deviceIndexString, out deviceIndex) ||
                     deviceIndex < 1 || deviceIndex > allDevices.Count)
                 {
@@ -48,46 +42,40 @@ namespace ConsoleApp1
                 }
             } while (deviceIndex == 0);
 
-            // Take the selected adapter
-            PacketDevice selectedDevice = allDevices[deviceIndex - 1];
+            var selectedDevice = allDevices[deviceIndex - 1];
 
-            // Open the device
-            using (PacketCommunicator communicator =
-                selectedDevice.Open(65536,                                  // portion of the packet to capture
-                                                                            // 65536 guarantees that the whole packet will be captured on all the link layers
-                                    PacketDeviceOpenAttributes.Promiscuous, // promiscuous mode
-                                    1000))                                  // read timeout
+            using (var communicator = selectedDevice.Open(65536, PacketDeviceOpenAttributes.Promiscuous, 1000))  
             {
-                // Check the link layer. We support only Ethernet for simplicity.
                 if (communicator.DataLink.Kind != DataLinkKind.Ethernet)
                 {
                     Console.WriteLine("This program works only on Ethernet networks.");
                     return;
                 }
 
-                // Compile the filter
-                using (BerkeleyPacketFilter filter = communicator.CreateFilter("ip and udp"))
+                using (var filter = communicator.CreateFilter("ip and udp"))
                 {
-                    // Set the filter
                     communicator.SetFilter(filter);
                 }
 
                 Console.WriteLine("Listening on " + selectedDevice.Description + "...");
 
-                // start the capture
                 communicator.ReceivePackets(0, PacketHandler);
             }
         }
 
-        // Callback function invoked by libpcap for every incoming packet
         private static void PacketHandler(Packet packet)
         {
-            IpV4Datagram ip = packet.Ethernet.IpV4;
-            UdpDatagram udp = ip.Udp;
+            var ip = packet.Ethernet.IpV4;
+            var udp = ip.Udp;
 
-            // print ip addresses and udp ports
-            Console.WriteLine(packet.Timestamp.ToString("yyyy-MM-dd hh:mm:ss.fff") + " " + ip.Source + ":" + udp.SourcePort + " -> " + ip.Destination + ":" + udp.DestinationPort);
-            Console.WriteLine(udp.Payload);
+            if (udp.SourcePort == 27015)
+            {
+                // print ip addresses and udp ports
+                Console.WriteLine(packet.Timestamp.ToString("yyyy-MM-dd hh:mm:ss.fff") + " " + ip.Source + ":" + udp.SourcePort + " -> " + ip.Destination + ":" + udp.DestinationPort);
+                Console.WriteLine(udp.Payload);
+
+                //udp.Payload.ToMemoryStream()
+            }
         }
     }
 }
---
+++ ConsoleApp1/Test/Program.cs
@@ -0,0 +1,57 @@
+﻿using CSERLibrary.Models;
+using System;
+using System.IO;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encrypt();
+            Decrypt();
+        }
+
+        private static void Decrypt()
+        {
+            var ice = new IceKey(2);
+            ice.Set(new byte[] { 0x43, 0x53, 0x47, 0x4F, 0xCC, 0x34, 0x00, 0x00, 0x33, 0x0D, 0x00, 0x00, 0x4C, 0x03, 0x00, 0x00 });
+
+            using (var reader = new BinaryReader(File.OpenRead("out.ice")))
+            {
+                byte[] temp = new byte[reader.BaseStream.Length];
+                int bytesLeft = (int)reader.BaseStream.Length;
+                while (bytesLeft >= 8)
+                {
+                    byte[] tmp = new byte[8];
+                    byte[] buffer = reader.ReadBytes(8);
+                    ice.Decrypt(buffer, ref tmp);
+                    bytesLeft -= 8;
+                    Array.Copy(tmp, temp, 8);
+                }
+                File.WriteAllBytes("plain1.txt", temp);
+            }
+        }
+
+        private static void Encrypt()
+        {
+            var ice = new IceKey(2);
+            ice.Set(new byte[] { 0x43, 0x53, 0x47, 0x4F, 0xCC, 0x34, 0x00, 0x00, 0x33, 0x0D, 0x00, 0x00, 0x4C, 0x03, 0x00, 0x00 });
+
+            using (var reader = new BinaryReader(File.OpenRead("plain.txt")))
+            {
+                byte[] temp = new byte[reader.BaseStream.Length];
+                int bytesLeft = (int)reader.BaseStream.Length;
+                while (bytesLeft >= 8)
+                {
+                    byte[] tmp = new byte[8];
+                    byte[] buffer = reader.ReadBytes(8);
+                    ice.Encrypt(buffer, ref tmp);
+                    bytesLeft -= 8;
+                    Array.Copy(tmp, temp, 8);
+                }
+                File.WriteAllBytes("out.ice", temp);
+            }
+        }
+    }
+}
