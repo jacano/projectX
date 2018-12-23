ebbaf9367d8bee49be34769047faf2611304f70f
Juan Antonio Cano <jacanosalado@gmail.com>
2018-12-24 00:54:30 +0100

    progress

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static readonly byte[] iceKey = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+        static readonly byte[] iceKey = new byte[] { 0x6C, 0x06, 0x5F, 0xA4, 0x05, 0xAD, 0x18, 0x60, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
         static void Main(string[] args)
         {
@@ -77,10 +77,23 @@ namespace ConsoleApp1
                 return;
             }
 
-            HandlePayload(payload);
+            using (var ms = payload.ToMemoryStream())
+            {
+                var payloadData = ms.ToArray();
+
+                SavePayload(payloadData);
+                //HandlePayload(payloadData);
+            }
         }
 
-        private static void HandlePayload(Datagram payload)
+        private static void SavePayload(byte[] payload)
+        {
+            var guid = Guid.NewGuid().ToString();
+
+            File.WriteAllBytes(guid, payload);
+        }
+
+        private static void HandlePayload(byte[] payload)
         {
             var ice = new IceKey(2);
             ice.Set(iceKey);
@@ -91,7 +104,7 @@ namespace ConsoleApp1
 
             using (var plaintext = new MemoryStream(payload.Length))
             {
-                using (var ciphertext = payload.ToMemoryStream())
+                using (var ciphertext = new MemoryStream(payload))
                 {
                     while (true)
                     {
@@ -110,17 +123,22 @@ namespace ConsoleApp1
 
                 plaintext.Seek(0, SeekOrigin.Begin);
 
-                var parser = new DemoParser(plaintext);
-                parser.TickDone += (object sender, TickDoneEventArgs e) =>
-                {
-                    foreach (var playingParticipants in parser.PlayingParticipants)
-                    {
-                        Console.WriteLine($"{playingParticipants.Name} - position: {playingParticipants.Position}");
-                    }
-                };
-
-                parser.ParseToEnd();
+                //ParseDemo(plaintext);
             }
+        }
+
+        private static void ParseDemo(MemoryStream plaintext)
+        {
+            var parser = new DemoParser(plaintext);
+            parser.TickDone += (object sender, TickDoneEventArgs e) =>
+            {
+                foreach (var playingParticipants in parser.PlayingParticipants)
+                {
+                    Console.WriteLine($"{playingParticipants.Name} - position: {playingParticipants.Position}");
+                }
+            };
+
+            parser.ParseToEnd();
         }
     }
 }
