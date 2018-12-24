3b1e7248e89cca4fccb3f8cfc65c3811b7820b62
Juan Antonio Cano <jacanosalado@gmail.com>
2018-12-24 12:54:18 +0100

    cleanup

--- proto/make_cc_files.bat
+++ proto/make_cc_files.bat
@@ -1,7 +1,7 @@
 @if not exist generated_proto mkdir generated_proto
 
 @echo Running Protocol Buffer Compiler on netmessages_public.proto...
-@protoc-3.6.1-win32\bin\protoc.exe --proto_path=public\ --proto_path=protobuf-3.6.1\src --cpp_out=generated_proto netmessages_public.proto
+@protoc-3.6.1-win32\bin\protoc.exe --proto_path=public\ --cpp_out=generated_proto netmessages_public.proto
 
 @echo Running Protocol Buffer Compiler on cstrike15_usermessages_public.proto...
-@protoc-3.6.1-win32\bin\protoc.exe --proto_path=public\ --proto_path=protobuf-3.6.1\src --cpp_out=generated_proto cstrike15_usermessages_public.proto
+@protoc-3.6.1-win32\bin\protoc.exe --proto_path=public\ --cpp_out=generated_proto cstrike15_usermessages_public.proto
