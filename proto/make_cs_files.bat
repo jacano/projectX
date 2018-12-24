fe54b620a5fea20c9534e2db9b8b1d40d4f82108
Juan Antonio Cano <jacanosalado@gmail.com>
2018-12-24 12:23:29 +0100

    progress

---
+++ proto/make_cs_files.bat
@@ -0,0 +1,7 @@
+@if not exist generated_proto_cs mkdir generated_proto_cs
+
+@echo Running Protocol Buffer Compiler on netmessages_public.proto...
+@protoc-3.6.1-win32\bin\protoc.exe --proto_path=internal\ --proto_path=protobuf-3.6.1\src --csharp_out=generated_proto_cs netmessages.proto
+
+@echo Running Protocol Buffer Compiler on cstrike15_usermessages_public.proto...
+@protoc-3.6.1-win32\bin\protoc.exe --proto_path=internal\ --proto_path=protobuf-3.6.1\src --csharp_out=generated_proto_cs cstrike15_usermessages.proto
