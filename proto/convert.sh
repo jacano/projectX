0a1ac58a71654610f5814f8bd367be6033bcddb8
Juan Antonio Cano <jacanosalado@gmail.com>
2018-12-24 16:35:46 +0100

    progress with protogen

--- proto/convert.sh
+++
@@ -1,4 +0,0 @@
-mkdir proto3
-
-protoc --plugin=protoc-gen-proto2_to_proto3 --proto2_to_proto3_out=proto3/ --proto_path=internal/ netmessages.proto
-protoc --plugin=protoc-gen-proto2_to_proto3 --proto2_to_proto3_out=proto3/ --proto_path=internal/ cstrike15_usermessages.proto
